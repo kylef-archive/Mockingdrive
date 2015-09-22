1e758ceaafb50872504ea046da734bdc207a0610
Kyle Fuller <kyle@fuller.li>
2015-09-22 09:23:34 -0700

    Initial project

---
+++ Mockingdrive/Mockingdrive.h
@@ -0,0 +1,7 @@
+@import Foundation;
+
+//! Project version number for Mockingdrive.
+FOUNDATION_EXPORT double MockingdriveVersionNumber;
+
+//! Project version string for Mockingdrive.
+FOUNDATION_EXPORT const unsigned char MockingdriveVersionString[];
